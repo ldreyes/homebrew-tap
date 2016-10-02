c7ef92d91ca8a8e9b4926fca5503b4a4bbbae576
Leslie Reyes <ldianereyes@gmail.com>
2016-10-02 18:13:27 -0400

    updated new release to have updated script with permissions

--- git-undo.rb
+++ git-undo.rb
@@ -5,7 +5,7 @@
 class GitUndo < Formula
     desc "A terminal wrapper around Git that supports undo of commonly used Git commands"
     homepage "https://github.com/juliexue/git-undo"
-    url "https://github.com/juliexue/git-undo/archive/v0.2.tar.gz"
+    url "https://github.com/juliexue/git-undo/archive/v0.3.tar.gz"
     version "0.1"
     sha256 "bf64c8639533e3f7154b9e9a5004a55b2608502564e1bc8dcf1dd0cbcf51a5b4"
 
