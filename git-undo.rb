1907dd30c388e4a409693866769c3d8b5a6828a7
Leslie Reyes <ldianereyes@gmail.com>
2016-10-02 18:56:42 -0400

    typo

--- git-undo.rb
+++ git-undo.rb
@@ -5,7 +5,7 @@
 class GitUndo < Formula
     desc "A terminal wrapper around Git that supports undo of commonly used Git commands"
     homepage "https://github.com/juliexue/git-undo"
-    url "https://lsgithub.com/juliexue/git-undo/archive/v0.3.tar.gz"
+    url "https://github.com/juliexue/git-undo/archive/v0.3.tar.gz"
     version "0.3"
     sha256 "8bcc1723c021987499d68c5f561ce0da2e4a681d"
 
