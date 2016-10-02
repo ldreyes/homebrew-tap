38411431ef48294ee5b138ff03fa8a2ff99926ee
Leslie Reyes <ldianereyes@gmail.com>
2016-10-02 18:54:04 -0400

    update sha256

--- git-undo.rb
+++ git-undo.rb
@@ -5,9 +5,9 @@
 class GitUndo < Formula
     desc "A terminal wrapper around Git that supports undo of commonly used Git commands"
     homepage "https://github.com/juliexue/git-undo"
-    url "https://github.com/juliexue/git-undo/archive/v0.2.tar.gz"
+    url "https://lsgithub.com/juliexue/git-undo/archive/v0.2.tar.gz"
     version "0.1"
-    sha256 "bf64c8639533e3f7154b9e9a5004a55b2608502564e1bc8dcf1dd0cbcf51a5b4"
+    sha256 "8bcc1723c021987499d68c5f561ce0da2e4a681d"
 
     def install
         bin.install "git-undo"
